23e97089b80c40ce02feec70d23b32eaf4d616ba
BDCraven <bencraven@hotmail.com>
2017-08-07 17:56:08 +0100

    Add passing test for MVP1: creating a listing

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,25 @@
 require 'sinatra/base'
 
 class MakersBnB < Sinatra::Base
+  enable :sessions
 
   get '/' do
     "infrastructure working"
   end
 
+  get '/listings' do
+    @name = session[:name]
+    erb :'listings'
+  end
+
+
+  get '/listings/new' do
+    erb :'new'
+  end
+
+  post '/listings' do
+    session[:name] = params[:name]
+    redirect '/listings'
+  end
+  run! if app_file == $0
 end
