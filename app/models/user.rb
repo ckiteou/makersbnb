9e9326a7cf4e28b1a6a1f0be67232143196077e8
BDCraven <bencraven@hotmail.com>
2017-08-09 17:23:40 +0100

    User can log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User
   property :last_name,  String
   property :email,      String, format: :email_address, required: true, unique: true
   property :password,   Text
+
+  def self.authenticate(email, password)
+    first(email: email)
+    # user = first(email: email)
+    # if user && BCrypt::password.new(user.password_digest) == password
+    #   user
+    # else
+    #   nil
+    # end
+  end
+
 end
